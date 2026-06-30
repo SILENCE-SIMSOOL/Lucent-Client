67511a14df6c4bfd8a9bd01fd889b61976c477a7
SILENCE-SIMSOOL <79349144+SILENCE-SIMSOOL@users.noreply.github.com>
2026-06-30 23:17:56 +0900

    use -> entityclick

--- src/main/java/silence/simsool/lucentclient/mixin/mixins/MixinMinecraft.java
+++ src/main/java/silence/simsool/lucentclient/mixin/mixins/MixinMinecraft.java
@@ -6,36 +6,16 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.world.InteractionHand;
 import silence.simsool.lucentclient.mods.impl.graphics.AnimationsMod;
 
 @Mixin(Minecraft.class)
 public abstract class MixinMinecraft {
 
-	@Redirect(
-		method = "startUseItem",
-		at = @At(
-			value = "INVOKE",
-			target = "Lnet/minecraft/client/player/LocalPlayer;swing(Lnet/minecraft/world/InteractionHand;)V"
-		)
-	)
+	@Redirect(method = "startUseItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;swing(Lnet/minecraft/world/InteractionHand;)V", ordinal = 0))
 	private void redirectSwing(LocalPlayer player, InteractionHand hand) {
-		if (!(AnimationsMod.isEnabled() && AnimationsMod.DisableUseAnimation)) {
+		if (!(AnimationsMod.isEnabled() && AnimationsMod.DisableEntityClickAnimation)) {
 			player.swing(hand);
-		}
-	}
-
-	@Redirect(
-		method = "startUseItem",
-		at = @At(
-			value = "INVOKE",
-			target = "Lnet/minecraft/client/renderer/ItemInHandRenderer;itemUsed(Lnet/minecraft/world/InteractionHand;)V"
-		)
-	)
-	private void redirectItemUsed(ItemInHandRenderer itemInHandRenderer, InteractionHand hand) {
-		if (!(AnimationsMod.isEnabled() && AnimationsMod.DisableUseAnimation)) {
-			itemInHandRenderer.itemUsed(hand);
 		}
 	}
 

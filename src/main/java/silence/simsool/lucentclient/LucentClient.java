a6c86ef3037f180bf8e8210a086a2c37fc7c4842
SILENCE-SIMSOOL <79349144+SILENCE-SIMSOOL@users.noreply.github.com>
2026-09-02 14:50:46 +0900

    dd

--- src/main/java/silence/simsool/lucentclient/LucentClient.java
+++ src/main/java/silence/simsool/lucentclient/LucentClient.java
@@ -21,7 +21,7 @@ public class LucentClient implements ClientModInitializer {
 
 	public static final String ID = "lucentclient";
 	public static final String NAME = "Lucent Client";
-	public static final String VERSION = "1.1.6";
+	public static final String VERSION = "1.1.7";
 
 	public static final String PREFIX = "§b[§fLucent Client§b] ";
 
